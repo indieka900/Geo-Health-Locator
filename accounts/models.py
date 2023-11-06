ea5ccfde398d7d681e6baaa6c3c9fde375b24ec4
Kerriea-star <ojared511@gmail.com>
2023-11-06 05:52:16 +0300

    migrated accounts' models

--- accounts/models.py
+++ accounts/models.py
@@ -86,7 +86,7 @@ class User(AbstractBaseUser, TrackingModel):
     last_name = models.CharField(_('last name'),
                             max_length=150, blank=False, null=False
                             )
-    identification = models.IntegerField(_("identification"), max_length=100, blank=False, null=False)
+    identification = models.IntegerField(_("identification"), blank=False, null=False)
     email = models.EmailField(_('email'), unique=True, error_messages={
         'unique': ('A user with email already exists.'),
     })
