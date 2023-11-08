0b3d8a01cf14612d6d4e016fe0c1de6eb74ce675
Joseph <indiekaj@gmail.com>
2023-11-08 17:26:33 +0300

    login form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from accounts.views import MedicalPersonellSignupView, CommunityMemberSignupView,VerifyEmail
+from accounts.views import MedicalPersonellSignupView, Medicalpersonellogin, CommunityMemberSignupView,VerifyEmail, Communitymemberlogin
 
 app_name = 'accounts'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('community-member/', CommunityMemberSignupView.as_view(), name='register-community-member'),
     path('medical-personel-registration/', MedicalPersonellSignupView.as_view(), name='register-medical-personell'),
     path('activate/', VerifyEmail,name="email-verify"),
+    path('member-login/', Communitymemberlogin,name="login-member"),
+    path('medics-login/', Medicalpersonellogin,name="login-medics"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,54 +1,4 @@
-# from django.shortcuts import render, redirect
-# from .models import User
-# from .forms import UserForm
-
-# def create_user(request):
-#     if request.method == 'POST':
-#         user_form = UserForm(request.POST)
-#         if user_form.is_valid():
-#             user = user_form.save()
-#             # You can add any additional processing or redirection here.
-#             return redirect('/')
-#     else:
-#         user_form = UserForm()
-    
-#     return render(request, 'usercreationform.html', {'user_form': user_form})
-
-# def create_administrator(request):
-#     if request.method == 'POST':
-#         admin_form = AdministratorForm(request.POST)
-#         if admin_form.is_valid():
-#             administrator = admin_form.save()
-#             # You can add any additional processing or redirection here.
-#             return redirect('administrator_created')
-#     else:
-#         admin_form = AdministratorForm()
-    
-#     return render(request, 'create_administrator.html', {'admin_form': admin_form})
-
-# def create_community_member(request):
-#     if request.method == 'POST':
-#         member_form = MedicalPersonelForm(request.POST)
-#         if member_form.is_valid():
-#             community_member = member_form.save()
-#             # You can add any additional processing or redirection here.
-#             return redirect('/')
-#     else:
-#         member_form = MedicalPersonelForm()
-    
-#     return render(request, 'communityRegister.html', {'member_form': member_form})
-
-# def create_medical_personel(request):
-#     if request.method == 'POST':
-#         medical_form = MedicalPersonelForm(request.POST)
-#         if medical_form.is_valid():
-#             medical_personel = medical_form.save()
-#             # You can add any additional processing or redirection here.
-#             return redirect('/')
-#     else:
-#         medical_form = MedicalPersonelForm()
-    
-#     return render(request, 'create_medical_personel.html', {'medical_form': medical_form})
+import jwt
 
 from accounts.decorators import administrator_required, community_member_required
 from accounts.forms import (MedicalPersonnelSignUpForm,
@@ -64,11 +14,8 @@ from django.views.decorators.cache import never_cache
 from django.views.generic import CreateView
 from django.conf import settings
 from django.contrib import messages
-import jwt
-# from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-# from rest_framework_simplejwt.tokens import RefreshToken
-# from rest_framework_simplejwt.views import (TokenObtainPairView,
-#                                             TokenRefreshView)
+from django.contrib.auth import authenticate, login, logout
+
 
 decorators = [never_cache, login_required, administrator_required]
 
@@ -117,6 +64,56 @@ class CommunityMemberSignupView(CreateView):
             community_member.save()
             send_activation_mail(user, self.request)
         return render(self.request, "sign_alert.html")
+
+def Communitymemberlogin(request):
+    
+    # if request.user.is_authenticated:
+    #     return redirect('/')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            member= CommunityMember.objects.get(user__email=email)
+        except CommunityMember.DoesNotExist:
+            messages.error(request, 'email does not exist!') 
+        
+        member = authenticate(request, email=email, password=password)
+        
+        if member is not None:
+            login(request, member)
+            messages.success(request, 'Logged in succesfully')
+            return redirect('/')
+        else:
+            messages.error(request, 'email or password does not exist')
+            return redirect('/login')
+    return render(request,'login.html',{'member':'member'})
+
+
+def Medicalpersonellogin(request):
+    
+    # if request.user.is_authenticated:
+    #     return redirect('/')
+    if request.method == 'POST':
+        kmdb_mumber = request.POST.get('kmdb_mumber')
+        password = request.POST.get('password')
+        try:
+            medics= MedicalPersonel.objects.get(kmdb_mumber=kmdb_mumber)
+        except MedicalPersonel.DoesNotExist:
+            messages.error(request, 'email does not exist!') 
+        
+        medics = authenticate(request, kmdb_mumber=kmdb_mumber, password=password)
+        
+        if medics is not None:
+            login(request, medics)
+            messages.success(request, 'Logged in succesfully')
+            return redirect('/')
+        else:
+            messages.error(request, 'email or password does not exist')
+            return redirect('/login')
+    
+    return render(request,'login.html',{'medics':'medics'})
+
+
 
 def RequestPasswordReset(request):
     context = {
