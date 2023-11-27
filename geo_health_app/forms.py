d84a5e963bf50d91f29d84f0013138f423adbb1c
Kerriea-star <ojared511@gmail.com>
2023-11-27 12:01:56 +0300

    treatment form

--- geo_health_app/forms.py
+++ geo_health_app/forms.py
@@ -21,3 +21,17 @@ class OrderAmbulanceForm(forms.ModelForm):
     class Meta:
         model = Patient
         exclude = ["reporter"]
+
+class TreatPatientForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": 'form-control'})
+
+    class Meta:
+        model = TreatPatient
+        fields = ("full_name", "op_number", "age", "height",
+                    "bp_reading", "glucose_level", "weight_reading",
+                    "temperature_reading", "symptoms", "prescribe_lab_test" 
+                    # "lab_test_results", "drug_prescription", "treatment_status"
+                    )
